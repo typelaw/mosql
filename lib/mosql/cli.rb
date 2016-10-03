2baf24af6bb98d637d27daa178f02968e210ddde
andrew <andrew.baker@live.com>
2016-10-03 13:47:02 -0700

    Trying to fix mosql for mongo 4

--- lib/mosql/cli.rb
+++ lib/mosql/cli.rb
@@ -123,7 +123,7 @@ module MoSQL
     def connect_mongo
       puts 'calling mongoClient'
       @mongo = Mongo::Client.new(options[:mongo])
-      config = @mongo['admin'].command(:ismaster => 1)
+      config = @mongo['admin'].command({ismaster => 1})
       if !config['setName'] && !options[:skip_tail]
         log.warn("`#{options[:mongo]}' is not a replset.")
         log.warn("Will run the initial import, then stop.")
